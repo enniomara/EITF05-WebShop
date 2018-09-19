fd4a5a0653a253fe3891bfe762363b9534cb28ea
theonebigblue <nilsson.041880837@hotmail.com>
2018-09-19 15:49:21 +0200

    Start on the site page

--- src/index.php
+++ src/index.php
@@ -1,5 +1,74 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+  <head>
 
-?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>???</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Webbshop</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Varukorg</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Jumbotron Header -->
+      <header class="jumbotron my-4">
+        <h1 class="display-3"><center>A Warm Welcome!</center></h1>
+        <p class="lead">Vi säljer bla..bla...bla</p>
+      </header>
+
+      <!-- Product rendering -->
+      <div class="row text-center">
+
+        <?php
+            include('webbshop.php');
+        ?>
+      </div>
+
+    </div>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+  </body>
+
+</html>
---
+++ src/webbshop.php
@@ -0,0 +1,19 @@
+
+<?php
+$item = "test item";
+
+echo'
+<div class="col-lg-4 col-md-5 mb-4">
+  <div class="card">
+    <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+    <div class="card-body">
+      <h4 class="card-title">',$item,'</h4>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+    </div>
+    <div class="card-footer">
+      <a href="#" class="btn btn-primary">Find Out More!</a>
+    </div>
+  </div>
+</div>';
+
+?>
