e45d9b3fa39d6cd241c5504a36ca0681fc188d52
chrillebile <chrillebile@gmail.com>
2018-09-24 19:19:48 +0200

    add session to index

--- src/public/index.php
+++ src/public/index.php
@@ -3,13 +3,16 @@
 require_once __DIR__ . '/../App/autoload.php';
 
 use App\Classes\DBConnection;
+use App\Classes\SessionManager;
 
 try {
     $dbh = new DBConnection();
+    $session = new SessionManager();
     $items = [];
     foreach ($dbh->query('SELECT * from users') as $row) {
         $items[] = $row;
     }
+    $session::startSession('test');
     echo '<pre>';
     print_r($items);
     echo '</pre>';
