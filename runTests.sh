af5ede577fa0f9cac357fd642ed900a90cd2faf6
Ennio Mara <enniomara@users.noreply.github.com>
2018-09-27 14:28:42 +0200

    Further testing of travis

--- runTests.sh
+++ runTests.sh
@@ -2,4 +2,4 @@
 
 ABSPATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-docker run -v "$ABSPATH"/src/:/app -w "/app" --rm epcallan/php7-testing-phpunit:7.2-phpunit7 phpunit --bootstrap App/autoload.php tests
+docker run -v "$ABSPATH"/src/:/app -w "/app" --rm -it epcallan/php7-testing-phpunit:7.2-phpunit7 phpunit --configuration phpunit.xml --testsuite Unit
