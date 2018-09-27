fa9594b339392ed8efeb4ba2a4774ca6440a029b
chrillebile <chrillebile@gmail.com>
2018-09-27 12:38:58 +0200

    Add start session to index page

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,12 @@
 require_once __DIR__ . '/../App/global.php';
 
 try {
+    $dbh = new DBConnection();
+    $session = new SessionManager();
+
+    //Starting session
+    $session::startSession('Test');
+
     $items = [];
     foreach ($databaseConnection->query('SELECT * from users') as $row) {
         $items[] = $row;
