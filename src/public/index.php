233f66347271a1824947e9fdbfabd5c6959f5d1b
Christian Bilevits <chrillebile@users.noreply.github.com>
2018-09-28 20:06:49 +0200

    Feature session manager (#9)
    - Add session manager
    
    * add ignore for vs code
    
    * add interface for Session Manager
    
    * add base structure for a session manger *NOTE: should be updated
    
    * add session to index
    
    * add ignore for vs code
    
    * add interface for Session Manager
    
    * add base structure for a session manger *NOTE: should be updated
    
    * Add destroy session to interface
    
    * stopHijack is now preventHijacking. Add timeout_duration const
    
    * Implemented a basic session manager. You can now start a basic session, validate if a session needs to be destroyed and destroy a session.
    
    * Add start session to index page
    
    * Add session manager connection
    
    * Resolves https://github.com/enniomara/EITF05-WebShop/pull/9\#discussion_r220916408
    
    * changes static methods to public
    
    * remove unnecessary closing tag
    
    * Added simple doc
    
    * add returns
    
    * remove preventHijacking
    
    * preventHijacking now private
    
    * change :: to -> becuase none-static
    
    * remove startSession parameters
    
    * sorten method names
    
    * add method isStarted
    
    * timeout_duration and started now private
    
    * namechange
    
    * fix name

--- src/public/index.php
+++ src/public/index.php
@@ -2,11 +2,19 @@
 
 require_once __DIR__ . '/../App/global.php';
 
+use App\Classes\SessionManager;
+
 try {
+    $session = new SessionManager();
+
+    //Starting session
+    $session->start('Test');
+
     $items = [];
     foreach ($databaseConnection->query('SELECT * from users') as $row) {
         $items[] = $row;
     }
+    
     echo '<pre>';
     print_r($items);
     echo '</pre>';
