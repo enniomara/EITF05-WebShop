7e0743cba0f87eb80684a1bdea5d3afa7060bbbf
chrillebile <chrillebile@gmail.com>
2018-09-28 15:13:52 +0200

    Added simple doc

--- src/App/Interfaces/SessionManagerInterface.php
+++ src/App/Interfaces/SessionManagerInterface.php
@@ -2,15 +2,39 @@
 
 namespace App\Interfaces;
 
+/**
+ * Session manager interface
+ */
 interface SessionManagerInterface {
 
+    /**
+     * For easies starting a session and creating a cookie.
+     */
     public function startSession($name, $limit = 0, $path = '/', $domain = null, $secure = null);
 
+    /**
+     * Prevent hijackers from hijacking a session.
+     */
     public function preventHijacking();
 
+    /**
+     * Updating current session ID with a new one.
+     * 
+     * @return bool if succeeded
+     */
     public function regenerateSession();
 
+    /**
+     * Validates if current session is legit and still should exist.
+     * 
+     * @return bool true if session is valid and false if session isn't
+     */
     public function validateSession($time);
 
+    /**
+     * Removing a session and its content.
+     * 
+     * @return bool true if success and false if not.
+     */
     public function destroySession();
 }
