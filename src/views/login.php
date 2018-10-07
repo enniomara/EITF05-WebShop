3289484f4396493c6e4fb5f059e6fb2413e8c432
Ennio Mara <enniomara@users.noreply.github.com>
2018-10-07 16:01:22 +0200

    Represent HTML content as MVC views (#32)
    Instead of having view (html output), in public files (e.g. login.php), move the view logic to a separate file. That way the project gets more MVCish, and the public files become easier to read.

---
+++ src/views/login.php
@@ -0,0 +1,26 @@
+<html>
+<?php include("head.php"); ?>
+<body>
+    <h1>Login:</h1>
+    <form action="login.php?action=login" name="login" method="POST">
+        <label for="username">Username:</label> <input type="text" name="username">
+        <br />
+        <label for="password">Password:</label> <input type="password" name="password">
+        <br />
+        <button type="submit">Submit</button>
+    </form>
+
+    <h1>Sign up:</h1>
+    <form action="login.php?action=signup" name="signup" method="POST">
+        <label for="username">Username:  <input type="text" name="username"></label>
+        <br />
+        <label for="password">Password: <input type="password" name="password"></label>
+        <br />
+        <label for="address">Address:  <input type="text" name="address"></label>
+        <br />
+        <button type="submit">Submit</button>
+    </form>
+    
+    <?php include("footer.php"); ?>
+</body>
+</html>
