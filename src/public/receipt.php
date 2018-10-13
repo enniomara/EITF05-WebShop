40caf982e8e870c809444311ea3b7467a3af6b7e
chrillebile <chrillebile@gmail.com>
2018-10-13 16:01:36 +0200

    Returning 404 page when user is not set

--- src/public/receipt.php
+++ src/public/receipt.php
@@ -1,6 +1,12 @@
 <?php
 require('../App/global.php');
 
+// Checking if user is not set
+if(!$sessionManager->isUserSet()){
+    echo "<h1>404</h1><p>Not found</p>";
+    return;
+}
+
 use App\Classes\View;
 use App\Classes\OrderService;
 use App\Classes\DAO\OrderMySQLDAO;
@@ -16,26 +22,25 @@ $itemService = new ItemService($itemDAO);
 
 // Getting id from http header
 $orderId = intval($_GET['orderId']);
-$view = new View('receipt');
 
-if($sessionManager->isUserSet()){
-    // Finding orders
-    $orders = $orderService->findOrder($orderId);
+// Finding orders
+$orders = $orderService->findOrder($orderId);
 
-    // Adding the orders to a cart for easier handeling
-    $cart = new Cart();
-    foreach ($orders as $order){
-        $cart->addItem($itemService->findAllByIds($order['itemId'])[0], $order['amount']);
-    }
-
-    // Getting cart items
-    $cartItems = $cart->getItems();
-
-    $view->setAttribute('title', 'Receipt#' . $orderId);
-    $view->setAttribute('loggedInUser', $loggedInUser);
-    $view->setAttribute('cart', $cart);
-    $view->setAttribute('cartItems', $cartItems);
-    $view->setAttribute('orderId', $orderId);
+// Adding the orders to a cart for easier handeling
+$cart = new Cart();
+foreach ($orders as $order){
+    $cart->addItem($itemService->findAllByIds($order['itemId'])[0], $order['amount']);
 }
+
+// Getting cart items
+$cartItems = $cart->getItems();
+
+$view = new View('receipt');
+$view->setAttribute('title', 'Receipt#' . $orderId);
+$view->setAttribute('loggedInUser', $loggedInUser);
+$view->setAttribute('cart', $cart);
+$view->setAttribute('cartItems', $cartItems);
+$view->setAttribute('orderId', $orderId);
+
 
 echo $view->render();
