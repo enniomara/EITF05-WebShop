860d0df96914a70e16dea7272cacc9c7127cc6d6
chrillebile <chrillebile@gmail.com>
2018-10-13 16:28:15 +0200

    Fix escape

--- src/views/receipt/itemRow.php
+++ src/views/receipt/itemRow.php
@@ -1,5 +1,5 @@
 <tr>
-    <td class="col-md-9"><em><?php echo escape($item->getName()); ?></em></h4></td>
+    <td class="col-md-9"><em><?php echo $item->getName(); ?></em></h4></td>
     <td class="col-md-1" style="text-align: center"><?php echo intval($cart->getAmount($item)); ?></td>
     <td class="col-md-1 text-center"><?php echo intval($item->getPrice()); ?></td>
     <td class="col-md-1 text-center"><?php echo intval($item->getPrice()) * intval($cart->getAmount($item)); ?></td>
