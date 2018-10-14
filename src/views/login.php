c4bf611e9e012f6686f8b30e64515926eb05dedf
chrillebile <chrillebile@gmail.com>
2018-10-14 23:54:59 +0200

    fix password hint text

--- src/views/login.php
+++ src/views/login.php
@@ -27,9 +27,9 @@
                 <form action="login.php?action=signup" name="signup" method="POST">
                     <label for="username">Username:  <input type="text" name="username"></label>
                     <br />
-                    <label for="password">Password: <input type="password" name="password"></label>
-                    <br />
-                    <label for="address">Address:  <input type="text" name="address"></label>
+                    <label for="password">Password: <input type="password" name="password" aria-describedby="passwordHelp"></label>
+                    <small id="passwordHelp" class="form-text text-muted">Minimum 7 characters. One upper and one lower case. One number. One special character.</small>
+                    <label for="address">Home Address:  <input type="text" name="address"></label>
                     <br />
                     <button class="btn btn-primary" type="submit">Submit</button>
                     <input type="hidden" name="token" value="<?php echo $CSRFToken ?>">
